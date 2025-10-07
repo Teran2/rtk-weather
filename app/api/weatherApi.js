3821e8c086b9d14962d8a92f829df45487cfc91c
Teran <teranarthur99@gmail.com>
2025-10-07 06:34:25 -0700

    fixed url, eslint and prettier config added.

--- app/api/weatherApi.js
+++ app/api/weatherApi.js
@@ -1,15 +1,17 @@
 export async function fetchWeather(city, apiKey) {
   // I just learned about encodeURIComponent, it's useful for properly formatting the url from user inputs and prevents the browser from misinterpreting.
   const q = encodeURIComponent(city);
-  const url = `api.openweathermap.org/data/2.5/forecast?q=${q}&appid=${apiKey}&units=imperial`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&appid=${apiKey}&units=imperial`;
 
-  const res = await fetch(url); // await is used to pause the execution of the function until the promise is resolved.
-  const body = await res.json().catch(() => ({})); // It parses the response as JSON, if not it will return an empty object as safeguard.
+  const res = await fetch(url);
+
+  // It parses the response as JSON, if not it will return an empty object as safeguard.
+  const body = await res.json().catch(() => ({}));
 
   // This checks the response status, if the status is not in the range of 200-299, it will handle the error.
   if (!res.ok) {
     // openweathermap api will include error messages in the JSON response if present.
-    const message = body?.message || "Failed to fetch weather"; // If message isn't provided, it defaults.
+    const message = body?.message || "Failed to fetch weather";
     throw new Error(message);
   }
 
